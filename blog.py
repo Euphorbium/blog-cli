a4df5c4f55fc751c789c79339032c6e0b1329929
Euphorbium <compositum@gmail.com>
2015-05-21 09:59:00 +0300

    pep8 cleanup

--- blog.py
+++ blog.py
@@ -1,40 +1,50 @@
 import argparse
 import sys
-import sqlalchemy  as sql
+import sqlalchemy as sql
 from sqlalchemy.exc import IntegrityError
 
+
 def parse_args(args):
-    parser = argparse.ArgumentParser(description='Manage your blog from command line',)
+    parser = argparse.ArgumentParser(
+        description='Manage your blog from command line',)
     subparsers = parser.add_subparsers(
         help='you should choose if you want to work with posts, or categories',
         dest='command')
-    category_parser = subparsers.add_parser('category', help="work with categories")
-    category_subparsers = category_parser.add_subparsers(help="add, list or assign a category to a post",
-                                                         dest='subcommand')
+    category_parser = subparsers.add_parser(
+        'category', help="work with categories")
+    category_subparsers = category_parser.add_subparsers(
+        help="add, list or assign a category to a post", dest='subcommand')
     category_add = category_subparsers.add_parser('add',)
     category_assign = category_subparsers.add_parser('assign',)
     category_assign.add_argument('post', type=int)
     category_assign.add_argument('category', type=int)
     category_list = category_subparsers.add_parser('list',)
-    category_list.add_argument('category', nargs='?', help='list posts of a given category. If no category is'
-                               'specified, lists all the categories', default=None)
+    category_list.add_argument(
+        'category', nargs='?', help='list posts of a given category. If no '
+        'category is specified, lists all the categories', default=None)
     category_add.add_argument('category',)
     post_parser = subparsers.add_parser('post', help="work with posts")
-    post_subparsers = post_parser.add_subparsers(help="add, list or search",  dest='subcommand')
+    post_subparsers = post_parser.add_subparsers(
+        help="add, list or search",  dest='subcommand')
     post_add = post_subparsers.add_parser('add',)
     post_list = post_subparsers.add_parser('list',)
     post_search = post_subparsers.add_parser('search',)
-    post_search.add_argument('search', help='search for blog posts containing given string')
-    post_add.add_argument('add', nargs=2, help="add a blog post with a given title", type=str)
-    post_add.add_argument('--category', help="assign blog post to a category,"
-                          "if category does not exist, it will be created", type=str, default=None)
+    post_search.add_argument(
+        'search', help='search for blog posts containing given string')
+    post_add.add_argument(
+        'add', nargs=2, help="add a blog post with a given title", type=str)
+    post_add.add_argument(
+        '--category', help="assign blog post to a category,"
+        "if category does not exist, it will be created",
+        type=str, default=None)
     return parser.parse_args(args)
+
 
 class Blog:
 
     def __init__(self, engine='sqlite:///blog.db'):
         try:
-            self.db=sql.create_engine(engine, echo=False)
+            self.db = sql.create_engine(engine, echo=False)
         except:
             sys.stderr.write('failed to connect to a database')
             raise
@@ -45,36 +55,54 @@ class Blog:
 
         sql.event.listen(self.db, 'connect', _fk_pragma_on_connect)
 
-        self.posts_table=sql.Table('posts', self.metadata,
-                            sql.Column('id', sql.Integer, primary_key=True, ),
-                            sql.Column('title', sql.String(100),),
-                            sql.Column('content', sql.Text,),
-                            sqlite_autoincrement=True
-                            )
-        self.categories_table=sql.Table('categories', self.metadata,
-                            sql.Column('id', sql.Integer, primary_key=True,),
-                            sql.Column('name', sql.String(100), unique=True),
-                            sqlite_autoincrement=True
-                                )
-        self.categories_posts_table=sql.Table('categoriesPosts', self.metadata,
-                            sql.Column('id', sql.Integer, primary_key=True),
-                            sql.Column('post', sql.Integer, sql.ForeignKey('posts.id'), nullable=False),
-                            sql.Column('category', sql.Integer, sql.ForeignKey('categories.id'), nullable=False),
-                            sqlite_autoincrement=True
-                                )
+        self.posts_table = sql.Table(
+            'posts', self.metadata,
+            sql.Column(
+                'id', sql.Integer, primary_key=True, ),
+            sql.Column('title', sql.String(100),),
+            sql.Column('content', sql.Text,),
+            sqlite_autoincrement=True
+        )
+        self.categories_table = sql.Table(
+            'categories', self.metadata,
+            sql.Column(
+                'id', sql.Integer, primary_key=True,),
+            sql.Column(
+                'name', sql.String(100), unique=True),
+            sqlite_autoincrement=True
+        )
+        self.categories_posts_table = sql.Table(
+            'categoriesPosts', self.metadata,
+            sql.Column(
+                'id', sql.Integer,
+                primary_key=True),
+            sql.Column(
+                'post', sql.Integer,
+                sql.ForeignKey('posts.id'),
+                nullable=False),
+            sql.Column('category',
+                       sql.Integer, sql.ForeignKey(
+                           'categories.id'),
+                       nullable=False),
+            sqlite_autoincrement=True
+        )
         self.metadata.create_all()
-        conn=self.db.connect()
+        conn = self.db.connect()
 
     def post_add(self, title, content, category_name=None):
-        post_id=self.posts_table.insert().execute(title=title, content=content).inserted_primary_key[0]
+        post_id = self.posts_table.insert().execute(
+            title=title, content=content).inserted_primary_key[0]
         if category_name:
-            category=sql.select([self.categories_table]).where(
-            self.categories_table.c.name==category_name).execute().first()
+            category = sql.select([self.categories_table]).where(
+                self.categories_table.c.name == category_name).execute().first()
             if category:
-                self.categories_posts_table.insert().execute(post=post_id, category=category.id)
+                self.categories_posts_table.insert().execute(
+                    post=post_id, category=category.id)
             else:
-                category_id=self.categories_table.insert().execute(name=category_name,).inserted_primary_key[0]
-                self.categories_posts_table.insert().execute(post=post_id, category=category_id)
+                category_id = self.categories_table.insert().execute(
+                    name=category_name,).inserted_primary_key[0]
+                self.categories_posts_table.insert().execute(
+                    post=post_id, category=category_id)
         return post_id
 
     def post_list(self):
@@ -84,8 +112,8 @@ class Blog:
 
     def post_search(self, search):
         posts = sql.select([self.posts_table]).where(
-                            (self.posts_table.c.title.like('%{0}%'.format(search)))|\
-                            (self.posts_table.c.content.like('%{0}%'.format(search)))).execute()
+            (self.posts_table.c.title.like('%{0}%'.format(search))) |
+            (self.posts_table.c.content.like('%{0}%'.format(search)))).execute()
         for post in posts:
             print(post.id, '|', post.title, '|', post.content)
 
@@ -95,16 +123,20 @@ class Blog:
     def category_list(self, category=None):
         if category:
             posts = sql.select(
-                [self.posts_table, self.categories_table, self.categories_posts_table],
+                [self.posts_table, self.categories_table,
+                    self.categories_posts_table],
                 use_labels=True
             ).where(
-                (self.posts_table.c.id==self.categories_posts_table.c.post) &\
-                (self.categories_table.c.id==self.categories_posts_table.c.category) &\
+                (self.posts_table.c.id == self.categories_posts_table.c.post) &
+                (self.categories_table.c.id ==
+                    self.categories_posts_table.c.category) &
                 (self.categories_table.c.name == category)
             ).execute()
             if posts:
                 for post in posts:
-                    print(post.posts_id, '|', post.posts_title, '|', post.posts_content)
+                    print(
+                        post.posts_id, '|', post.posts_title, '|',
+                        post.posts_content)
             else:
                 print('No posts found')
         else:
@@ -114,31 +146,31 @@ class Blog:
 
     def category_assign(self, post, category):
         try:
-            self.categories_posts_table.insert().execute(post=post, category=category)
+            self.categories_posts_table.insert().execute(
+                post=post, category=category)
         except IntegrityError:
             print('post or category with given id does not exist')
-
 
 
 def main():
     args = parse_args(sys.argv[1:])
     blog = Blog()
-    if args.command=='post':
-        if args.subcommand=='add':
+    if args.command == 'post':
+        if args.subcommand == 'add':
             title = args.add[0]
             content = args.add[1]
             blog.post_add(title, content, category_name=args.category)
-        if args.subcommand=='list':
+        if args.subcommand == 'list':
             blog.post_list()
-        if args.subcommand=='search':
+        if args.subcommand == 'search':
             blog.post_search(args.search)
-    elif args.command=='category':
-        if args.subcommand=='add':
+    elif args.command == 'category':
+        if args.subcommand == 'add':
             blog.category_add(args.category)
-        elif args.subcommand=='list':
+        elif args.subcommand == 'list':
             blog.category_list(args.category)
-        elif args.subcommand=='assign':
+        elif args.subcommand == 'assign':
             blog.category_assign(args.post, args.category)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
--- tests.py
+++ tests.py
@@ -5,14 +5,18 @@ import sqlalchemy as sql
 from blog import parse_args, Blog
 from nose.tools import assert_equals
 
+
 def test_parse_args():
     parsed = parse_args(['post', 'add', 'title', 'content'])
     assert_equals(argparse.Namespace(
-        command='post', subcommand='add', category=None, add=['title', 'content']), parsed)
-    parsed = parse_args(['post', 'add', 'title', 'content', '--category', 'category'])
+        command='post', subcommand='add', category=None,
+        add=['title', 'content']), parsed)
+    parsed = parse_args(['post', 'add', 'title', 'content', '--category',
+                         'category'])
     assert_equals(argparse.Namespace(
-        command='post', subcommand='add', category='category', add=['title', 'content']), parsed)
-    parsed = parse_args(['post', 'list',])
+        command='post', subcommand='add', category='category',
+        add=['title', 'content']), parsed)
+    parsed = parse_args(['post', 'list', ])
     assert_equals(argparse.Namespace(
         command='post', subcommand='list',), parsed)
     parsed = parse_args(['post', 'search', 'word'])
@@ -21,12 +25,13 @@ def test_parse_args():
     parsed = parse_args(['category', 'add', 'category-name'])
     assert_equals(argparse.Namespace(
         command='category', subcommand='add', category='category-name'), parsed)
-    parsed = parse_args(['category', 'list',])
+    parsed = parse_args(['category', 'list', ])
     assert_equals(argparse.Namespace(
         command='category', subcommand='list', category=None), parsed)
     parsed = parse_args(['category', 'list', 'category-name'])
     assert_equals(argparse.Namespace(
-        command='category', subcommand='list', category='category-name'), parsed)
+        command='category', subcommand='list', category='category-name'),
+        parsed)
     parsed = parse_args(['category', 'assign', '2', '5'])
     assert_equals(argparse.Namespace(
         command='category', subcommand='assign', post=2, category=5), parsed)
@@ -35,7 +40,7 @@ def test_parse_args():
 class TestBlog():
 
     def setUp(self):
-        self.blog=Blog(engine='sqlite://')
+        self.blog = Blog(engine='sqlite://')
         self.savedstdout = sys.stdout
         sys.stdout = io.StringIO()
 
@@ -50,25 +55,32 @@ class TestBlog():
         assert_equals(expected_result, real_result)
 
     def test_post_add_category(self):
-        self.blog.post_add('test post', 'test content', category_name='test category')
+        self.blog.post_add('test post', 'test content',
+                           category_name='test category')
         expected_posts = [(1, 'test post', 'test content')]
         expected_categories = [(1, 'test category', )]
         expected_categories_posts = [(1, 1, 1)]
         real_posts = list(sql.select([self.blog.posts_table]).execute())
-        real_categories = list(sql.select([self.blog.categories_table]).execute())
-        real_categories_posts = list(sql.select([self.blog.categories_posts_table]).execute())
+        real_categories =\
+            list(sql.select([self.blog.categories_table]).execute())
+        real_categories_posts = list(
+            sql.select([self.blog.categories_posts_table]).execute())
         assert_equals(expected_posts, real_posts)
         assert_equals(expected_categories, real_categories)
         assert_equals(expected_categories_posts, real_categories_posts)
 
     def test_post_list(self):
-        self.blog.posts_table.insert().execute(title='test title', content='test content')
+        self.blog.posts_table.insert().execute(
+            title='test title', content='test content')
         self.blog.post_list()
-        assert_equals(sys.stdout.getvalue().strip(), '1 | test title | test content')
+        assert_equals(
+            sys.stdout.getvalue().strip(), '1 | test title | test content')
 
     def test_post_search(self):
-        self.blog.posts_table.insert().execute(title='test title', content='test content')
-        self.blog.posts_table.insert().execute(title='second', content='2nd post')
+        self.blog.posts_table.insert().execute(
+            title='test title', content='test content')
+        self.blog.posts_table.insert().execute(
+            title='second', content='2nd post')
         self.blog.posts_table.insert().execute(title='third', content='3rd')
         self.blog.post_search('second')
         assert_equals(sys.stdout.getvalue().strip(), '2 | second | 2nd post')
@@ -87,13 +99,14 @@ class TestBlog():
         assert_equals(expected_result, real_result)
 
     def test_category_assign(self):
-        self.blog.posts_table.insert().execute(title='test title', content='test content')
+        self.blog.posts_table.insert().execute(
+            title='test title', content='test content')
         self.blog.categories_table.insert().execute(name='test category')
         self.blog.category_assign(1, 1)
         expected_result = [(1, 1, 1)]
-        real_result = list(sql.select([self.blog.categories_posts_table]).execute())
+        real_result = list(
+            sql.select([self.blog.categories_posts_table]).execute())
         assert_equals(expected_result, real_result)
         self.blog.category_assign(1, 2)
-        assert_equals(sys.stdout.getvalue().strip(), 
-        'post or category with given id does not exist')
-
+        assert_equals(sys.stdout.getvalue().strip(),
+                      'post or category with given id does not exist')
